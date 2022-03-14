3f7b5d12e5cbebc5c79518b53e2ce6a52ec63441
LuisDev <luisziquinha2019@gmail.com>
2022-03-13 23:18:56 -0300

    Conversor de moedas

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('#form')
+form.addEventListener('submit', function (e){
+e.preventDefault()
+
+const inputDolar = form.querySelector('#dolarValue');
+const dolar = inputDolar.value;
+if(!dolar){
+    setResult('Valor inválido', false)
+    return
+}
+
+const calculationDolar = conversionDolar(dolar)
+const msg = calculationDolar;
+setResult(msg,true)
+})
+
+function conversionDolar (dolar){
+    
+    const calculationDolar = dolar * 5.08;
+    return calculationDolar.toFixed(2)
+
+}
+function criaP (){
+    const p = document.createElement('p');
+    return p 
+}
+function setResult(msg,isValid){
+const result = document.querySelector('#resultConversion');
+result.innerHTML = '';
+const p = criaP ();
+p.innerHTML = msg;
+if(isValid){p.classList.add('text')}
+else{p.classList.add('bad')}
+result.appendChild(p)
+}
